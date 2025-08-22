5f479f99abddec83a251c551e77786ad565281d2
Alexis Cerullo <alexis.cerullo@gmail.com>
2025-08-22 10:35:21 -0300

    Se agregó funcionalidad al botón de compra y se pasaron los productos a un json externo para consumirlo con fetch

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-const hamburguesas = [{
-    nombre: "Simple", precio: 9000, imagen: "simple.png"
-}, {
-    nombre: "Doble", precio: 13000, imagen: "doble.png"
-}, {
-    nombre: "Cheddar", precio: 10000, imagen: "cheddar.png"
-}, {
-    nombre: "Completa", precio: 12000, imagen: "completa.png"
-}, {
-    nombre: "Pollo", precio: 9000, imagen: "pollo.png"
-}, {
-    nombre: "Veggie", precio: 10000, imagen: "veggie.png"
-}];
-
 let cuenta = JSON.parse(localStorage.getItem("cuenta")) || [];
 let total = cuenta.reduce((acumulacion, hamburguesa) => acumulacion + hamburguesa.precio, 0);
 
@@ -23,6 +9,19 @@ const acumulacion = document.getElementById("acumulacion");
 const cuentaTotal = document.createElement('p');
 cuentaTotal.classList.add('cuenta-total');
 contenedorCuenta.appendChild(cuentaTotal);
+
+const botonCompra = document.getElementById('boton-compra');
+botonCompra.addEventListener('click', () => {
+    if (total === 0) {
+        alert('agrega algo al carro')
+    } else {
+        cuenta = [];
+        guardarCuenta();
+        mostrarCuenta();
+        alert('su compra ha sido finalizada')
+    }
+
+});
 
 function guardarCuenta() {
     localStorage.setItem("cuenta", JSON.stringify(cuenta));
@@ -56,39 +55,46 @@ function mostrarCuenta() {
 
 mostrarCuenta();
 
+async function obtenerHamburguesas() {
+    try {
+        const response = await fetch("hamburguesas.json");
+        const hamburguesas = await response.json();
+        hamburguesas.forEach(hamburguesa => {
 
-hamburguesas.forEach(hamburguesa => {
+            const item = document.createElement('div');
+            item.classList.add('hamburguesa');
+            contenedorHamburguesas.appendChild(item);
 
-    const item = document.createElement('div');
-    item.classList.add('hamburguesa');
-    contenedorHamburguesas.appendChild(item);
+            const nombre = document.createElement('h3');
+            nombre.classList.add('hamburguesa-nombre');
+            nombre.textContent = hamburguesa.nombre;
+            item.appendChild(nombre);
 
-    const nombre = document.createElement('h3');
-    nombre.classList.add('hamburguesa-nombre');
-    nombre.textContent = hamburguesa.nombre;
-    item.appendChild(nombre);
+            const precio = document.createElement('p');
+            precio.classList.add('hamburguesa-precio');
+            precio.textContent = `$${hamburguesa.precio}`;
+            item.appendChild(precio);
 
-    const precio = document.createElement('p');
-    precio.classList.add('hamburguesa-precio');
-    precio.textContent = `$${hamburguesa.precio}`;
-    item.appendChild(precio);
+            const imagen = document.createElement('img');
+            imagen.classList.add('hamburguesa-imagen');
+            imagen.setAttribute("src", `images/${hamburguesa.imagen}`);
+            imagen.setAttribute("alt", `Imagen de una hamburguesa ${hamburguesa.nombre}`);
+            item.appendChild(imagen);
 
-    const imagen = document.createElement('img');
-    imagen.classList.add('hamburguesa-imagen');
-    imagen.setAttribute("src", `images/${hamburguesa.imagen}`);
-    imagen.setAttribute("alt", `Imagen de una hamburguesa ${hamburguesa.nombre}`);
-    item.appendChild(imagen);
+            const botonAgregar = document.createElement('input');
+            botonAgregar.classList.add('hamburguesa-boton');
+            botonAgregar.setAttribute("type", `button`);
+            botonAgregar.setAttribute("value", `Agregar a la Cuenta`);
+            item.appendChild(botonAgregar);
 
-    const botonAgregar = document.createElement('input');
-    botonAgregar.classList.add('hamburguesa-boton');
-    botonAgregar.setAttribute("type", `button`);
-    botonAgregar.setAttribute("value", `Agregar a la Cuenta`);
-    item.appendChild(botonAgregar);
-
-    botonAgregar.addEventListener('click', () => {
-        cuenta.push({ nombre: hamburguesa.nombre, precio: hamburguesa.precio });
-        guardarCuenta();
-        mostrarCuenta();
-    });
-});
-
+            botonAgregar.addEventListener('click', () => {
+                cuenta.push({ nombre: hamburguesa.nombre, precio: hamburguesa.precio });
+                guardarCuenta();
+                mostrarCuenta();
+            });
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+obtenerHamburguesas();
